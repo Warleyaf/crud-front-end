cdb033af0be22ac4afdbc534804ac7a023b1ba0c
warleyaf <warleysilva365@gmail.com>
2022-06-30 18:54:55 -0300

    Adicionando os States do email e do password

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { useState } from "react";
 
 import './LoginPage.css';
 
 export function LoginPage() {
 
+   const [email, setEmail] = useState('');
+   const [password, setPassword] = useState('');
    const handleLogin = () => {
+      console.log('Email', email);
+      console.log('Password', password);
       console.log('Clicando em login')
    }
 
@@ -14,12 +19,24 @@ export function LoginPage() {
          <div className="form">
             <div className="field">
                <label htmlFor="email">Email:</label>
-               <input type="email" name="email" id="email" />
+               <input 
+                  type="email" 
+                  name="email" 
+                  id="email" 
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  />
             </div>
 
             <div className="field">
                <label htmlFor="password">Senha:</label>
-               <input type="password" name="password" id="password" />
+               <input 
+                  type="password" 
+                  name="password" 
+                  id="password" 
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  />
             </div>
 
             <div className="actions">
