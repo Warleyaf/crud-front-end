c403a01e027865cfafb52f4827fe7a0a62118198
warleyaf <warleysilva365@gmail.com>
2022-06-29 22:30:26 -0300

    Subindo a estilização do Login

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
+import './LoginPage.css';
+
 export function LoginPage() {
+
+   const handleLogin = () => {
+      console.log('Clicando em login')
+   }
+
    return(
-      <h1>Login Page</h1>
+      <div id="login">
+         <div className="title"><h1>Login</h1></div>
+         <div className="form">
+            <div className="field">
+               <label htmlFor="email">Email:</label>
+               <input type="email" name="email" id="email" />
+            </div>
+
+            <div className="field">
+               <label htmlFor="password">Senha:</label>
+               <input type="password" name="password" id="password" />
+            </div>
+
+            <div className="actions">
+               <button onClick={handleLogin}>Entrar</button>
+            </div>
+         </div>
+      </div>
    )
 }
