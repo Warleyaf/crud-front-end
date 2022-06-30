2d2ca81fd4d93d211eea12ac7af883f347f0a233
warleyaf <warleysilva365@gmail.com>
2022-06-30 19:44:53 -0300

    Adicionando a navbar e o campo de buscar e os estilos deles junto com as funções

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 
+import './MainPage.css';
+
 export function MainPage() {
+
+   const handleLogout = () => {
+      console.log('Logout')
+   }
+
+   const handleSearch = (query) => {
+      console.log('query', query);
+   }
+
+   const handleClear = () => {
+      console.log('clear');
+   }
+
    return(
-      <h1>Main Page</h1>
+      <div id="main">
+         <div className="nav">
+            <h1 className="logo">SisRepo</h1>
+            <button onClick={handleLogout}>Sair</button>
+         </div>
+
+         <div className="search">
+            <label htmlFor="query">Procurar</label>
+            <input type="search" name="query" id="query" />
+            <button onClick={handleClear}>Limpar</button>
+            <button onClick={handleSearch}>Procurar</button>
+         </div>
+
+
+      </div>
    )
 }
